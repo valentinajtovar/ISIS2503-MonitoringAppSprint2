c863a7ea75cd8bba1b23f97c3465e436a411d17e
valentina.jtovar <lv.jimenez@uniandes.edu.co>
2025-09-21 21:09:06 -0500

    1

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Order(models.Model):
+    id = models.CharField(primary_key=True, max_length=64)
+    status = models.CharField(max_length=32)
+    updated_at = models.DateTimeField(auto_now=True)
+    version = models.IntegerField(default=0)  # control optimista
+
+    def __str__(self):
+        return f"{self.id}:{self.status}:{self.version}"
